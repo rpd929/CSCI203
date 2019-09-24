49716e791b522f60912c35bf9b6facd259c8aafe
rpd929 <37601686+rpd929@users.noreply.github.com>
2019-09-24 15:51:17 +1000

    Update ex6.java

--- ex6.java
+++ ex6.java
@@ -1,19 +1,31 @@
+/*
+Exercise 5
+Rory Davis
+rpd929
+*/
 
 import java.io.*;
+import java.util.*;
 
 public class ex6 {
 
     public static final int SIZE_OF_BUFF  = 100;
     public static final int CHAR_NUM = 4;  // All Characters A, C, G, and T
-    public static final int prime =  7;  // prime number to use
+    public static final int prime =  7;  //PRIME NUMBER TO USE FOR MODS
 
 
     public static void main(String[] args)
     {
         char[] TARGET = new char[5000];
-        char[] S = new char[10];
+        char[] SEARCH = new char[10];
 
-        File file = new File("ex6.txt"); 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter file name");
+
+        String fileName = scan.nextLine();
+        scan.close();
+
+        File file = new File(fileName); 
         try {
 
             BufferedReader br = new BufferedReader(new FileReader(file)); 
@@ -25,67 +37,60 @@ public class ex6 {
 
                 String newLine2 = br.readLine();
 
-                S = newLine2.toCharArray();
-
-                System.out.println(TARGET);
-                System.out.println(S);
+                SEARCH = newLine2.toCharArray();
 
             }
             br.close();
 
-            rabin_karp_search(TARGET, S);
+           RK_search(TARGET, SEARCH);
         } catch( Exception e) {
-                System.out.println("here" + e.getMessage());
+                System.out.println(e.getMessage());
             }    
        
     
     }
 
-    /* A string pattern matching search using the Rabin-Karp Algorithm
-    * :param TARGET: Char array to search.
-    * :param P: Char array pattern to match.
-    * :param prime: A prime number to use in hashing.
-    * */
-    public static void rabin_karp_search(char[] TARGET, char[] S)
+    
+    public static void RK_search(char[] TARGET, char[] SEARCH)
     {
-        int s_len = S.length;
-        int t_len = TARGET.length;
+        int s_size = SEARCH.length;
+        int t_size = TARGET.length;
         int i, j;
-        int p = 0;  // hash value for pattern
-        int t = 0;  // hash value for text string
+        int p = 0;  // HASH FOR PATTERN
+        int t = 0;  // HASH FOR TEXT STRING
 
-        // preprocessing the hash value of p
-        // the hash function is pow(CHAR_NUM, M - 1) mod prime
+        //CALCULATING HASH OF p
+        //HASH: pow(CHAR_NUM, M - 1) mod prime
         int h = 1;
-        for (int x = 0; x < s_len - 1; x++) {
+        for (int x = 0; x < s_size - 1; x++) {
             h = (h * CHAR_NUM) % prime;
         }
 
-        // preprocessing the hash value of pattern and first part of search
-        for (int x = 0; x < s_len; x++) {
-            p = (CHAR_NUM * p + S[x]) % prime;
+        // CALCULATING HASH OF PATTERN AND FIRST PART OF SEARCH
+        for (int x = 0; x < s_size; x++) {
+            p = (CHAR_NUM * p + SEARCH[x]) % prime;
             t = (CHAR_NUM * t + TARGET[x]) % prime;
         }
 
-        // matching
-        for (int x = 0; x <= t_len; x++) {
-            // check the value of the current substr and iterate over chars if they match
+        // MATCHING .... 
+        for (int x = 0; x <= t_size; x++) {
+            
+            // IF MATCHES, ITERATE OVER AND MATCH CHARACTERS DIRECTLY
             if (p == t) {
-                for (j = 0; j < s_len; j++) {
-                    if (TARGET[x+j] != S[j]) break;  // if the next char doesnt match, no match found
+                for (j = 0; j < s_size; j++) {
+                    if (TARGET[x+j] != SEARCH[j]) break;  // NO MATCH: THEREFORE BREAK
                 }
                 
-                if (j == s_len) {
-                   System.out.printf("%-3d ", x);
+                if (j == s_size) {
+                   System.out.print(x + ", ");
                 }
             }
 
-            // apply the hash function for the next substr by
-            // removing the leading digit and adding trailing digit
-            if (x < t_len - s_len) {  // N - M because if the remaining string < M then stop
-                t = (CHAR_NUM * (t - TARGET[x] * h) + TARGET[x + s_len]) % prime;  // rolling update
+            // HASH NEXT SUBSTRING BY TRIMMING FIRST CHARACTER AND ADDING NEXT CHARACTER
+            if (x < t_size - s_size) {  // N - M because if the remaining string < M then stop
+                t = (CHAR_NUM * (t - TARGET[x] * h) + TARGET[x + s_size]) % prime;  // rolling update
 
-                if (t < 0) t += prime;  // ensure t is always positive
+                if (t < 0) t += prime;  // MAKE T POSITIVE
             }
         }
     }
